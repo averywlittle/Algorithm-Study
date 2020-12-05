ad94a08c178fd50319841e3cd1d5d26f40834bb2
Avery Little <averywlittle@hey.com>
2020-12-05 23:45:00 +0000

    started implementing karatsuba multiplication

---
+++ PA_1.py
@@ -0,0 +1,8 @@
+
+# Karatsuba's algorithm is an efficient way to solve integer multiplication problems where the two inputs have an even number of digits
+def karatsuba(a, b):
+    result = a * b;
+    print(result);
+
+def has_even_digits(a):
+  return True;
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import new, algos
+import PA_1, time
 
-new.hello()
-algos.add(3, 5)
+a = 3141592653589793238462643383279502884197169399375105820974944592
+
+b = 2718281828459045235360287471352662497757247093699959574966967627
+
+# Timing is heavily dependent on internet connection
+# This will print the correct answer, but hopefully we can see it's slower
+initA = time.perf_counter()
+print(a * b)
+print(f'{time.perf_counter() - initA:0.9f} seconds')
+
+# new line
+print('\n')
+
+initB = time.perf_counter()
+PA_1.karatsuba(a, b)
+print(f'{time.perf_counter() - initB:0.9f} seconds')
